32dad60e87259ab62236c3ed3a3368de87bfb12b
Mike Morley <mpekim@gmail.com>
2016-11-05 17:07:39 -0400

    Create Playing_With_Python.py

---
+++ Playing_With_Python.py
@@ -0,0 +1,7 @@
+# This is a comment in Python.
+# Comments are done using the symbol '#'.
+
+# Author: Mike Morley
+# Date: November 5th, 2016
+
+# Program Purpose: Just messing around on GitHub, making a program in Python when I probably should be doing homework.
