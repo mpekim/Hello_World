8a32f58072b246fd03f2b172c86a4516f48429ac
Mike Morley <mpekim@gmail.com>
2016-11-05 17:53:45 -0400

    Update Playing_With_Python.py

--- Playing_With_Python.py
+++ Playing_With_Python.py
@@ -4,4 +4,15 @@
 # Author: Mike Morley
 # Date: November 5th, 2016
 
+# Credited Sources of Help:
+# - "Hello, World!" Python Website (Learn Python)
+# http://www.learnpython.org/en/Hello,_World!
+
 # Program Purpose: Just messing around on GitHub, making a program in Python when I probably should be doing homework.
+
+# 1. Hello World Program in Python. (NOTE: This program is done in Python 2)
+
+print "This line will be printed."
+
+
+
